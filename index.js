379b10664f67354b749d7e81b20e4ab53592bfb7
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-01-14 13:01:15 -0800

    added favorite button

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 //keeps track of all likes for each board game
 const likeCountObj ={}
+const favoriteTracker = {}
 
 function renderGames(gamesArray){
     gamesArray.forEach(game => {
@@ -26,42 +27,54 @@ function createGameCard(game){
 
     //name and rank of game
     const h2 = document.createElement('h2')
-    
     h2.innerHTML = game.rank + ": " + game.name
-    div.appendChild(h2)
+    
 
     //img of game
     const img = document.createElement('img');
     img.className = 'game-image'
     img.src = game.image_url
-    div.appendChild(img)
+    
 
     //like button text
     const p = document.createElement('p');
     likeCountObj[game.name] = 0
     p.innerHTML = likeCountObj[game.name] + ' Likes'
-    div.appendChild(p)
+    
 
-    //like button
-    const button = document.createElement('button')
-    button.className = 'like-btn'
-    button.id = game.handle
-    button.innerHTML = 'Like &#10084'
-    div.appendChild(button)
+    //create like button and add event listener
+    const likeButton = createLikeButton()
 
-    button.addEventListener('click', () => {
+    likeButton.addEventListener('click', () => {
         likeCountObj[game.name]++
         p.innerHTML = likeCountObj[game.name] + ' Likes'
     })
     
-    
+    //create favorite button and add event listener to favorite and unfavorite games
+    const favoriteButton = createFavoirteButton()
+    favoriteTracker[game.name] = false
+    favoriteButton.addEventListener('click', () => {
+        if (favoriteTracker[game.name] === false){
+            favoriteButton.innerHTML = '&#9733'
+            favoriteButton.style = "color:yellow"
+            favoriteTracker[game.name] = true
+        }
+        else if (favoriteTracker[game.name] === true){
+            favoriteButton.innerHTML = '&#9734'
+            favoriteButton.style = "color:black"
+            favoriteTracker[game.name] = false
+        }
+        
+    })
+
     // div to contain all comments for each game
     const commentContainer = document.createElement('div')
     commentContainer.className = 'comment-container'
 
+    
     const commentTitle = document.createElement('p')
     commentTitle.id = 'comment-title'
-    commentTitle.innerHTML = 'Comments'
+    commentTitle.innerHTML = game.name + ' Comments'
     commentContainer.appendChild(commentTitle)
 
     // create form
@@ -69,13 +82,19 @@ function createGameCard(game){
     form.addEventListener('submit', (event) => {
         event.preventDefault()
         let p = document.createElement('p')
+        p.className = 'userComments'
         p.innerHTML = `${event.target.commentInput.value}`
         commentContainer.appendChild(p)
         form.reset()
     })
 
 
-    // add comment form and comment form container to game
+    // append all elements to the game div
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+    div.appendChild(likeButton)
+    div.appendChild(favoriteButton)
     div.appendChild(form)
     div.appendChild(commentContainer)
 
@@ -83,7 +102,20 @@ function createGameCard(game){
     gameCollection.appendChild(div)
 }
 
+function createLikeButton(){
+    const button = document.createElement('button')
+    button.className = 'like-btn'
+    button.innerHTML = 'Like &#10084'
+    return button
+}
 
+function createFavoirteButton(){
+    const button = document.createElement('button')
+    button.className = 'favorite-btn'
+    button.innerHTML = '&#9734'
+    button.style = "color:black"
+    return button
+}
 
 
 function createCommentForm(){
@@ -91,11 +123,13 @@ function createCommentForm(){
     const form = document.createElement('form')
     form.className = 'commentForm'
 
-    const comment = document.createElement('input')
+    const comment = document.createElement('textarea')
     // comment input box
-    comment.type = 'text'
+    comment.id = 'textInput'
     comment.name = 'commentInput'
     comment.placeholder = 'Comment on game...'
+    comment.rows = 3
+    comment.cols = 30
 
     // break line element
     const br = document.createElement('br')
